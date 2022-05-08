54c540b381c25d8144c234b1e5dea6425c56face
Josh <josh@creativeworks.com.au>
2022-05-08 19:40:54 +0800

    Added gps_travel package
    This gps_travel package is on the bot at ~/Desktop/Group4Proj/catkin_ws/src/gps_travel.
    The group4.launch file is the preliminary launch file, it launches the master node, aria, gps and gps_travel. So far gps_travels only use is to start driving forward, but it is in the process of being worked on

--- src/gps_travel/scripts/GPSTravel.py
+++ src/gps_travel/scripts/GPSTravel.py
@@ -1,56 +1,55 @@
 #!/usr/bin/env python
 
 # Import required Python code.
-import roslib
-roslib.load_manifest('traveller')
 import rospy
 import sys
 
-
 # Import custom message data and dynamic reconfigure variables.
-from geometry_msgs.Twist import travel
+from geometry_msgs.msg import Twist
 
 # Node example class.
 class GPSTravel():
     # Must have __init__(self) function for a class, similar to a C++ class constructor.
     def __init__(self):
         # Get the ~private namespace parameters from command line or launch file.
-        topic = rospy.get_param('~topic', 'chatter')
+        topic = rospy.get_param('~topic', 'RosAria/cmd_vel')
         rospy.loginfo('topic = %s', topic)
         # Create a dynamic reconfigure server.
         # Create a publisher for our custom message.
-        pub = rospy.Publisher(topic, travel)
+        pub = rospy.Publisher(topic, Twist, queue_size=10)
         # Set the message to publish as our custom message.
-        msg = travel()
+        msg = Twist()
         # Initialize message variables.
-        msg.a = 1
-        msg.b = 2
-        msg.message = 'joe biden'
+        msg.linear.x = 1
+        msg.linear.y = 0
+        msg.linear.z = 0
+        msg.angular.x = 0
+        msg.angular.y = 0
+        msg.angular.z = 0
+
         # Main while loop.
         while not rospy.is_shutdown():
             # Fill in custom message variables with values from dynamic reconfigure server.
-            msg.message = self.message
-            msg.a = self.a
-            msg.b = self.b
+#            msg.linear = self.linear
+#            msg.angular = self.angular
             # Publish our custom message.
             pub.publish(msg)
             # Sleep for a while before publishing new messages. Division is so rate != period.
 
             rospy.sleep(1.0)
 
-    # Create a callback function for the dynamic reconfigure server.
-    def reconfigure(self, config, level):
-        # Fill in local variables with values received from dynamic reconfigure clients (typically the GUI).
-        self.message = config["message"]
-        self.a = config["a"]
-        self.b = config["b"]
-        # Return the new variables.
-        return config
+#    # Create a callback function for the dynamic reconfigure server.
+#    def reconfigure(self, config, level):
+#        # Fill in local variables with values received from dynamic reconfigure clients (typically the GUI).
+#        self.linear = config["linear"]
+#        self.angular = config["angular"]
+#        # Return the new variables.
+#        return config
 
 # Main function.
 if __name__ == '__main__':
     # Initialize the node and name it.
-    rospy.init_node('pytalker')
+    rospy.init_node('GPSTravel')
     # Go to class functions that do all the heavy lifting. Do error checking.
     try:
         ne = GPSTravel()
